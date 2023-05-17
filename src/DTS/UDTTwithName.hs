bee71b7c4b634e0ff21180327190007a8756ac1a
Daisuke Bekki <bekki@is.ocha.ac.jp>
2023-05-17 13:42:40 +0900

    eliminated mfenced from DTS.UDTTwithName

--- src/DTS/UDTTwithName.hs
+++ src/DTS/UDTTwithName.hs
@@ -179,23 +179,23 @@ instance MathML Preterm where
     Con cname -> T.concat ["<mtext>", cname, "</mtext>"]
     Type -> "<mi>type</mi>"
     Kind -> "<mi>kind</mi>"
-    Pi vname a b -> T.concat ["<mrow><mfenced separators=':'>", toMathML vname, toMathML a, "</mfenced><mo>&rarr;</mo>", toMathML b, "</mrow>"]
+    Pi vname a b -> T.concat ["<mrow><mo>(</mo>", toMathML vname, "<mo>:</mo>", toMathML a, "<mo>&rarr;</mo>", toMathML b, "</mrow>"]
     Not a -> T.concat["<mrow><mi>&not;</mi>", toMathML a, "</mrow>"]
     Lam vname m -> T.concat ["<mrow><mi>&lambda;</mi>", toMathML vname, "<mpadded lspace='-0.2em' width='-0.2em'><mo>.</mo></mpadded>", toMathML m, "</mrow>"]
     App (App (Con cname) y) x ->
-      T.concat ["<mrow><mtext>", cname, "</mtext><mfenced>", toMathML x, toMathML y,"</mfenced></mrow>"]
+      T.concat ["<mrow><mtext>", cname, "</mtext><mo>(</mo>", toMathML x, toMathML y,"<mo>)</mo></mrow>"]
     App (App (App (Con cname) z) y) x ->
-      T.concat ["<mrow><mtext>", cname, "</mtext><mfenced>", toMathML x, toMathML y,toMathML z,"</mfenced></mrow>"]
+      T.concat ["<mrow><mtext>", cname, "</mtext><mo>(</mo>", toMathML x, toMathML y,toMathML z,"<mo>)</mo></mrow>"]
     App (App (App (App (Con cname) u) z) y) x ->
-      T.concat ["<mrow><mtext>", cname, "</mtext><mfenced>", toMathML x, toMathML y, toMathML z, toMathML u, "</mfenced></mrow>"]
-    App m n -> T.concat ["<mrow>", toMathML m, "<mfenced>", toMathML n, "</mfenced></mrow>"]
+      T.concat ["<mrow><mtext>", cname, "</mtext><mo>(</mo>", toMathML x, toMathML y, toMathML z, toMathML u, "<mo>)</mo></mrow>"]
+    App m n -> T.concat ["<mrow>", toMathML m, "<mo>(</mo>", toMathML n, "<mo>)</mo></mrow>"]
     Sigma vname a b -> case b of
                          Top -> toMathML a
                          _   -> T.concat ["<mrow><mo>[</mo><mtable columnalign='left'><mtr><mtd>", toMathML vname, "<mo>:</mo>", toMathML a, "</mtd></mtr><mtr><mtd><mpadded height='-0.5em'>", toMathML b, "</mpadded></mtd></mtr></mtable><mo>]</mo></mrow>"]
-    Pair m n  -> T.concat ["<mfenced>", toMathML m, toMathML n, "</mfenced>"]
-    Proj s m  -> T.concat ["<mrow><msub><mi>&pi;</mi>", toMathML s, "</msub><mfenced>", toMathML m, "</mfenced></mrow>"]
+    Pair m n  -> T.concat ["<mrow><mo>(</mo>", toMathML m, toMathML n, "<mo>)</mo></mrow>"]
+    Proj s m  -> T.concat ["<mrow><msub><mi>&pi;</mi>", toMathML s, "</msub><mo>(</mo>", toMathML m, "<mo>)</mo></mrow>"]
     Lamvec vname m  -> T.concat ["<mrow><mi>&lambda;</mi><mover>", toMathML vname, "<mo>&rarr;</mo></mover><mo>.</mo>", toMathML m, "</mrow>"]
-    Appvec vname m -> T.concat ["<mfenced separators=''>", toMathML m, "<mover>", toMathML vname, "<mo>&rarr;</mo></mover></mfenced>"]
+    Appvec vname m -> T.concat ["<mrow><mo>(</mo>", toMathML m, "<mover>", toMathML vname, "<mo>&rarr;</mo></mover><mo>)</mo></mrow>"]
     Unit       -> "<mi>()</mi>"
     Top        -> "<mi>&top;</mi>"
     Bot        -> "<mi>&bot;</mi>"
@@ -203,10 +203,10 @@ instance MathML Preterm where
     Nat    -> "<mi>N</mi>"
     Zero   -> "<mi>0</mi>"
     Succ n -> T.concat ["<mrow><mi>s</mi>", toMathML n, "</mrow>"]
-    Natrec n e f -> T.concat ["<mrow><mi>natrec</mi><mfenced>", toMathML n, toMathML e, toMathML f, "</mfenced></mrow>"]
+    Natrec n e f -> T.concat ["<mrow><mi>natrec</mi><mo>(</mo>", toMathML n, toMathML e, toMathML f, "<mo>)</mo></mrow>"]
     Eq a m n -> T.concat ["<mrow>", toMathML m, "<msub><mo>=</mo>", toMathML a, "</msub>", toMathML n, "</mrow>"]
-    Refl a m -> T.concat ["<mrow><mi>refl</mi>", toMathML a, "<mfenced>", toMathML m, "</mfenced></mrow>"]
-    Idpeel m n -> T.concat ["<mrow><mi>idpeel</mi><mfenced>", toMathML m, toMathML n, "</mfenced></mrow>"]
+    Refl a m -> T.concat ["<mrow><mi>refl</mi>", toMathML a, "<mo>(</mo>", toMathML m, "<mo>)</mo></mrow>"]
+    Idpeel m n -> T.concat ["<mrow><mi>idpeel</mi><mo>(</mo>", toMathML m, toMathML n, "<mo>)</mo></mrow>"]
     --DRel i t u v -> T.concat ["<mrow><msub><mi>DRel</mi><mn>", T.pack (show i), "</mn></msub><mtext>", t, "</mtext><mfenced>", toMathML u, toMathML v, "</mfenced></mrow>"]
 
 {- Judgment -}
@@ -221,7 +221,7 @@ instance Typeset Context where
   toTeX = (T.intercalate ",") . (map (\(nm,tm) -> T.concat [toTeX nm, ":", toTeX tm])) . reverse
 
 instance MathML Context where
-  toMathML cont = T.concat $ ["<mfenced separators=',' open='' close=''>"] ++ (map (\(nm,tm) -> T.concat ["<mrow>", toMathML nm, "<mo>:</mo>", toMathML tm, "</mrow>"]) $ reverse cont) ++ ["</mfenced>"]
+  toMathML cont = T.concat $ ["<mrow>"] ++ (map (\(nm,tm) -> T.intercalate "," T.concat [toMathML nm, "<mo>:</mo>", toMathML tm]) $ reverse cont) ++ ["</mrow>"]
 
 -- | prints a context vertically.
 toVerticalMathML :: Context -> T.Text
