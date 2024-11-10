218d060444e7f7c4956bc9d3db4ddf484e93f5a8
Daisuke Bekki <bekki@is.ocha.ac.jp>
2024-11-10 18:18:12 +0900

    Added k-variables

--- src/DTS/Index.hs
+++ src/DTS/Index.hs
@@ -16,6 +16,7 @@ module DTS.Index (
   , uIndex
   , xIndex
   , eIndex
+  , kIndex
   , initializeIndex
   ) where
 
@@ -28,14 +29,14 @@ import qualified Control.Monad as M       --base
 -- |   u for variables for propositions
 -- |   x for variables for entities
 -- |   e for variables for eventualities
-newtype Indexed a = Indexed { indexing :: Int -> Int -> Int -> Int -> (a,Int,Int,Int,Int) }
+-- |   k for variables for continuations
+newtype Indexed a = Indexed { indexing :: Int -> Int -> Int -> Int -> Int -> (a,Int,Int,Int,Int,Int) }
 
 instance Monad Indexed where
-  return m = Indexed (\s u x e -> (m,s,u,x,e))
-  (Indexed m) >>= f = Indexed (\s u x e -> let (m',s',u',x',e') = m s u x e;
-                                                   (Indexed n) = f m';
-                                               in
-                                               n s' u' x' e')
+  return m = Indexed (\s u x e k -> (m,s,u,x,e,k))
+  (Indexed m) >>= f = Indexed (\s u x e k -> let (m',s',u',x',e',k') = m s u x e k;
+                                                 (Indexed n) = f m';
+                                             in n s' u' x' e' k')
 
 instance Functor Indexed where
   fmap = M.liftM
@@ -46,17 +47,20 @@ instance M.Applicative Indexed where
 
 -- | A sequential number for variable names (i.e. x_1, x_2, ...) in a context
 sIndex :: Indexed Int
-sIndex = Indexed (\s u x e -> (s,s+1,u,x,e))
+sIndex = Indexed (\s u x e k -> (s,s+1,u,x,e,k))
 
 uIndex :: Indexed Int
-uIndex = Indexed (\s u x e -> (u,s,u+1,x,e))
+uIndex = Indexed (\s u x e k -> (u,s,u+1,x,e,k))
 
 xIndex :: Indexed Int
-xIndex = Indexed (\s u x e -> (x,s,u,x+1,e))
+xIndex = Indexed (\s u x e k -> (x,s,u,x+1,e,k))
 
 eIndex :: Indexed Int
-eIndex = Indexed (\s u x e -> (e,s,u,x,e+1))
+eIndex = Indexed (\s u x e k -> (e,s,u,x,e+1,k))
+
+kIndex :: Indexed Int
+kIndex = Indexed (\s u x e k -> (k,s,u,x,e,k+1))
 
 -- | re-assigns sequential indices to all asperands that appear in a given preterm.
 initializeIndex :: Indexed a -> a
-initializeIndex (Indexed m) = let (m',_,_,_,_) = m 0 0 0 0 in m'
+initializeIndex (Indexed m) = let (m',_,_,_,_,_) = m 0 0 0 0 0 in m'
