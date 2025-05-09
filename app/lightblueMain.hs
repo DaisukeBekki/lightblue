d0da9ebf7a1570cbc94bc0f8cae08b7e49a0376b
Mai MATSUBARA <g2020537@is.ocha.ac.jp>
2025-05-09 13:19:33 +0900

    add axiom 破くー破れる

--- app/lightblueMain.hs
+++ app/lightblueMain.hs
@@ -311,7 +311,7 @@ lightblueMain (Options lang commands style proverName filepath beamW nParse nTyp
         StrictT.putStrLn $ J.hypothesis j
         S.putStr "\n"
         let sentences = postpend (map T.fromStrict $ J.premises j) (T.fromStrict $ J.hypothesis j)
-            parseResult = NLI.parseWithTypeCheck parseSetting prover [("dummy",DTT.Entity)] [] sentences
+            parseResult = NLI.parseWithTypeCheck parseSetting prover [("dummy",DTT.Entity), ("ma1",DTT.Pi (DTT.Entity) (DTT.Pi (DTT.Entity) (DTT.Pi (DTT.Entity) (DTT.Pi (DTT.App (DTT.App (DTT.App (DTT.Con "破く/やぶく/ガヲ") (DTT.Var 0)) (DTT.Var 1)) (DTT.Var 2)) (DTT.Sigma (DTT.Entity) (DTT.App (DTT.App (DTT.Con "破れる/やぶれる/ガ") (DTT.Var 2)) (DTT.Var 0)))))))] [] sentences
         NLI.printParseResult handle style 1 noTypeCheck False title parseResult
         inferenceLabels <- toList $ NLI.trawlParseResult parseResult
         let groundTruth = J.jsemLabel2YesNo $ J.answer j
