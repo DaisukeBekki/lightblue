a9bd0fab2f0d539271895ba3d1a22f3fd0e00361
Koharu Saeki <koharu0929alexandros@gmail.com>
2025-11-21 13:41:44 +0900

    span parsing

--- src/Interface/Express/Express.hs
+++ src/Interface/Express/Express.hs
@@ -11,6 +11,7 @@ module Interface.Express.Express (
 
 import Yesod
 import qualified Data.Text.Lazy as T      --text
+import qualified Data.Text as TS          -- strict text for JSON
 import qualified Interface.Express.Lightblue as L
 import qualified Interface.Express.Sentence_process as SP
 import Data.List
@@ -26,9 +27,15 @@ import Control.Exception (catch, IOException)
 import System.Info (os)
 import Data.IORef
 import System.IO.Unsafe (unsafePerformIO)
-import qualified Parser.CCG as CCG (showScore, getLeafNodesFromNode)
+import qualified Parser.CCG as CCG (showScore, getLeafNodesFromNode, pf, cat)
+import qualified Parser.ChartParser as CP
+import qualified Parser.LangOptions as PL (defaultJpOptions)
+import qualified Parser.Language.Japanese.Lexicon as JP (setupLexicon)
+import qualified Parser.PartialParsing as Partial
+import qualified Data.Map as M
 import System.Environment (lookupEnv)
 import Text.Read (readMaybe)
+import Data.Aeson (Value, object, (.=))
 
 -- アプリケーションの状態として ParseResult を保持するための IORef を定義
 {-# NOINLINE currentParseResultRef #-}
@@ -57,6 +64,8 @@ data App = App
 
 mkYesod "App" [parseRoutes|
 /parsing ParsingR GET
+/span SpanR GET
+/span/node NodeR GET
 |]
 
 instance Yesod App
@@ -158,13 +167,21 @@ getParsingR = do
               <div id="parsing-view" style="display:block;">
                 <div class="header">
                   <div class="parsing-content">
-                    <div class="sentence-line">sentence: #{text_sen}
+                    <div class="sentence-line">sentence: <span id="sentence-text">#{text_sen}</span>
+                    <div class="span-controls">
+                      <input id="span-surface" type="text" placeholder="部分文字列をドラッグ選択">
+                      <input id="span-beam" type="number" value="32" min="1" max="256">
+                      <select id="span-results"></select>
+                      <span id="span-status"></span>
                     <div class="toggle-group">
                       <label for="cat-toggle" id="catbtn" class="toggle">cat
                       <label for="sem-toggle" id="sembtn" class="toggle">sem
 
                 <input type="checkbox" id="cat-toggle" :catChecked:checked/>
                 <input type="checkbox" id="sem-toggle" :semChecked:checked/>
+
+                <div id="span-preview-title" class="span-preview-title"></div>
+                <div id="span-preview-list" class="span-preview-list">
 
                 <div class="container-tab">
                   <div .tab-wrap>
@@ -193,6 +210,112 @@ getParsingR = do
             myDesign
             myFunction
  
+-- JSON: span parse results
+getSpanR :: Handler Value
+getSpanR = do
+  mpr <- liftIO $ readIORef currentParseResultRef
+  case mpr of
+    Nothing -> return $ object ["error" .= ("ParseResult is not set" :: TS.Text)]
+    Just pr -> do
+      let text_sen = case pr of
+                        NLI.SentenceAndParseTrees sentence _ -> sentence
+                        _ -> T.empty
+      mSurface <- lookupGetParam "surface"
+      mStart <- lookupGetParam "start"
+      mEnd <- lookupGetParam "end"
+      mBeam <- lookupGetParam "beam"
+      let beam :: Int
+          beam = case mBeam >>= (readMaybe . TS.unpack) of
+                   Just b -> b
+                   Nothing -> 32
+      chart <- liftIO $ L.parse beam text_sen
+      let entries = M.toList chart
+          normalize :: TS.Text -> TS.Text
+          normalize = TS.filter (\c -> not (c `elem` delims))
+          delims :: [Char]
+          delims = [' ', '　', '。', '、', '，', '．', ',', '.', '!', '！', '?', '？']
+      -- Token-based span finder: substring -> (i,j)
+      jpOptions <- liftIO PL.defaultJpOptions
+      (tokens, _) <- liftIO $ JP.setupLexicon jpOptions text_sen
+      let tokensN = map (normalize . T.toStrict) tokens
+          nTok = length tokensN
+          matchSpan :: TS.Text -> Maybe (Int,Int)
+          matchSpan target
+            | TS.null target = Nothing
+            | otherwise =
+                let go s e = if s >= nTok then Nothing
+                              else if e > nTok then go (s+1) (s+2)
+                              else let catse = TS.concat (take (e-s) (drop s tokensN)) in
+                                   if catse == target then Just (s,e) else go s (e+1)
+                in go 0 1
+          pickBySurface :: TS.Text -> Maybe ((Int,Int), [CP.Node])
+          pickBySurface surf =
+            let target = normalize surf in
+            let matches = [ (ij, ns)
+                          | (ij, ns) <- entries
+                          , not (null ns)
+                          , let pfs = map (normalize . T.toStrict . CCG.pf) ns
+                          , any (== target) pfs
+                          ] in
+            case matches of
+              (x:_) -> Just x
+              _ -> Nothing
+          pickByIndex :: Int -> Int -> [((Int,Int), [a])] -> Maybe ((Int,Int), [a])
+          pickByIndex i j = find (\((i',j'),_) -> i'==i && j'==j)
+      chosen <- case (mStart >>= readMaybe . TS.unpack, mEnd >>= readMaybe . TS.unpack) of
+                  (Just i, Just j) -> return $ pickByIndex i j entries
+                  _ -> case mSurface of
+                         Just surf ->
+                           case matchSpan (normalize surf) of
+                             Just (i,j) -> return $ pickByIndex i j entries
+                             Nothing    -> return $ pickBySurface surf
+                         Nothing -> return Nothing
+      case chosen of
+        Nothing -> do
+          -- Fallback: parse the substring independently
+          case mSurface of
+            Nothing -> return $ object ["nodes" .= ([] :: [TS.Text]), "message" .= ("No match" :: TS.Text)]
+            Just surf -> do
+              subChart <- liftIO $ L.parse beam (T.fromStrict surf)
+              let pr = Partial.extractParseResult beam subChart
+              case pr of
+                Partial.Failed -> return $ object ["nodes" .= ([] :: [TS.Text]), "message" .= ("No match" :: TS.Text)]
+                Partial.Full ns -> do
+                  let toNodeObj n = object
+                        [ "pf" .= (T.toStrict $ CCG.pf n)
+                        , "cat" .= (TS.pack $ show $ CCG.cat n)
+                        , "score" .= (T.toStrict $ CCG.showScore n)
+                        ]
+                  return $ object
+                    [ "pf" .= surf
+                    , "nodes" .= map toNodeObj ns
+                    , "subparse" .= True
+                    ]
+                Partial.Partial ns -> do
+                  let toNodeObj n = object
+                        [ "pf" .= (T.toStrict $ CCG.pf n)
+                        , "cat" .= (TS.pack $ show $ CCG.cat n)
+                        , "score" .= (T.toStrict $ CCG.showScore n)
+                        ]
+                  return $ object
+                    [ "pf" .= surf
+                    , "nodes" .= map toNodeObj ns
+                    , "subparse" .= True
+                    ]
+        Just ((i,j), ns) -> do
+          let toNodeObj n = object
+                [ "pf" .= (T.toStrict $ CCG.pf n)
+                , "cat" .= (TS.pack $ show $ CCG.cat n)
+                , "score" .= (T.toStrict $ CCG.showScore n)
+                ]
+          return $ object
+            [ "span" .= object ["start" .= i, "end" .= j]
+            , "pf" .= (T.toStrict $ CCG.pf (head ns))
+            , "nodes" .= map toNodeObj ns
+            , "subparse" .= False
+            ]
+
+
 --CSS（cassius）
 myDesign :: Widget
 myDesign = do
@@ -202,3 +325,82 @@ myDesign = do
 myFunction :: Widget
 myFunction = do
     toWidget $(juliusFile "src/Interface/Express/templates/express.julius")
+
+-- HTML snippet: render a single node like Leaf Node layout
+getNodeR :: Handler Html
+getNodeR = do
+  mpr <- liftIO $ readIORef currentParseResultRef
+  case mpr of
+    Nothing -> return [shamlet|<div class="error-message">ParseResult is not set|]
+    Just pr -> do
+      let text_sen = case pr of
+                        NLI.SentenceAndParseTrees sentence _ -> sentence
+                        _ -> T.empty
+      mStart <- lookupGetParam "start"
+      mEnd <- lookupGetParam "end"
+      mSurface <- lookupGetParam "surface"
+      mBeam <- lookupGetParam "beam"
+      mIndex <- lookupGetParam "index"
+      let parseInt :: TS.Text -> Maybe Int
+          parseInt = readMaybe . TS.unpack
+      let beam :: Int
+          beam = case mBeam >>= parseInt of
+                   Just b -> b
+                   Nothing -> 32
+          idx :: Int
+          idx = case mIndex >>= parseInt of
+                  Just i -> i
+                  Nothing -> 0
+      chart <- liftIO $ case (mStart >>= parseInt, mEnd >>= parseInt, mSurface) of
+                          (Just _, Just _, _) -> L.parse beam text_sen
+                          (_, _, Just surf)   -> L.parse beam (T.fromStrict surf)
+                          _                   -> L.parse beam text_sen
+      let entries = M.toList chart
+          normalize :: TS.Text -> TS.Text
+          normalize = TS.filter (\c -> not (c `elem` delims))
+          delims :: [Char]
+          delims = [' ', '　', '。', '、', '，', '．', ',', '.', '!', '！', '?', '？']
+      jpOptions <- liftIO PL.defaultJpOptions
+      (tokens, _) <- liftIO $ JP.setupLexicon jpOptions text_sen
+      let tokensN = map (normalize . T.toStrict) tokens
+          nTok = length tokensN
+          matchSpan :: TS.Text -> Maybe (Int,Int)
+          matchSpan target
+            | TS.null target = Nothing
+            | otherwise =
+                let go s e = if s >= nTok then Nothing
+                              else if e > nTok then go (s+1) (s+2)
+                              else let catse = TS.concat (take (e-s) (drop s tokensN)) in
+                                   if catse == target then Just (s,e) else go s (e+1)
+                in go 0 1
+          pickByIndex :: Int -> Int -> [((Int,Int), [a])] -> Maybe ((Int,Int), [a])
+          pickByIndex i j = find (\((i',j'),_) -> i'==i && j'==j)
+      chosen <- case (mStart >>= parseInt, mEnd >>= parseInt, mSurface) of
+                  (Just i, Just j, _) -> return $ pickByIndex i j entries
+                  (_, _, Just surf) ->
+                    case matchSpan (normalize surf) of
+                      Just (i,j) -> return $ pickByIndex i j entries
+                      Nothing    -> return Nothing
+                  _ -> return Nothing
+      nodeM <- case chosen of
+                 Just (_, ns) -> return $ if null ns then Nothing else (ns !!? idx)
+                 Nothing -> do
+                   case mSurface of
+                     Nothing -> return Nothing
+                     Just surf -> do
+                       subChart <- liftIO $ L.parse beam (T.fromStrict surf)
+                       let prSub = Partial.extractParseResult beam subChart
+                       case prSub of
+                         Partial.Failed   -> return Nothing
+                         Partial.Full ns  -> return $ ns !!? idx
+                         Partial.Partial ns -> return $ ns !!? idx
+      dsp <- liftIO $ readIORef currentDisplaySettingRef
+      let dspCollapsed = dsp { WE.defaultExpandDepth = 0 }
+      case nodeM of
+        Nothing -> defaultLayout $ do
+          [whamlet|<div class="error-message">No node|]
+        Just node -> defaultLayout $ do
+          WE.widgetizeWith dspCollapsed node
+  where
+    (!!?) :: [a] -> Int -> Maybe a
+    (!!?) xs n = if n < 0 || n >= length xs then Nothing else Just (xs !! n)
