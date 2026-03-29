38a058b946ec997d1db9ce0b9a76b6936b115769
Daisuke Bekki <bekki@is.ocha.ac.jp>
2026-03-29 18:47:19 +0900

    Remove the selectional restriction from

--- src/Parser/Language/English/Lexicon.hs
+++ src/Parser/Language/English/Lexicon.hs
@@ -77,9 +77,11 @@ terminator = UDTT.Ann (UDTT.Lam UDTT.Top) (DTT.Pi DTT.Entity DTT.Type)
 
 fromNLTKtoCCG :: NLTKword -> [Node]
 fromNLTKtoCCG (NLTKword word pos) = case (word,pos) of
+  ("Either",_) -> mylex ["Either"] "CC/DT" ((S []) `SL` (S []) `SL` CONJ `SL` (S [])) ((UDTT.Lam (UDTT.Lam (UDTT.Lam (UDTT.Lam (UDTT.App (UDTT.App (Var 2) (UDTT.App (Var 3) (Var 0))) (UDTT.App (Var 1) (Var 0))))))),[])
   (w,"NNP") -> mylex [w] "NNP" (NP []) ((Con w), [(w, DTT.Entity)])
   ("She","PRP") -> mylex ["She"] "PRP" (NP []) ((Proj Fst (Asp (Sigma Entity (Sigma Entity (App (App (Con "woman") (Var 1)) (Var 0)))))), [("woman", DTT.Pi DTT.Entity DTT.Type)])
-  ("she","PRP") -> mylex ["she"] "PRP" (NP []) ((Proj Fst (Asp (Sigma Entity (Sigma Entity (App (App (Con "woman") (Var 1)) (Var 0)))))), [("woman", DTT.Pi DTT.Entity DTT.Type)])
+  -- ("she","PRP") -> mylex ["she"] "PRP" (NP []) ((Proj Fst (Asp (Sigma Entity (Sigma Entity (App (App (Con "woman") (Var 1)) (Var 0)))))), [("woman", DTT.Pi DTT.Entity DTT.Type)])
+  ("she","PRP") -> mylex ["she"] "PRP" (NP []) ((Proj Fst (Asp Entity)), [])
   ("it","PRP") -> mylex ["it"] "PRP" (NP []) (UDTT.Con "it", [("it", DTT.Entity)])
   (w,"PRP") -> mylex [w] "PRP" (NP []) (UDTT.Con w, [(w, DTT.Entity)])
   --NP [] -- john
@@ -90,6 +92,7 @@ fromNLTKtoCCG (NLTKword word pos) = case (word,pos) of
   ("an","DT") -> mylex ["an"] "DT" ((T True 1 (S []) `SL` (T True 1 (S []) `BS` NP [])) `SL` N) 
         ((Lam (Lam (Lamvec (Sigma (Sigma Entity (App (App (Var 3) (Var 0)) terminator)) (Appvec 1 (App (Var 2) (Proj Fst (Var 0)))))))),[])
   ("the","DT") -> mylex ["the"] "DT" (NP [] `SL` N) ((Lam (Proj Fst (Asp (Sigma Entity (App (App (Var 1) (Var 0)) terminator))))), [])
+  ("The","DT") -> mylex ["The"] "DT" (NP [] `SL` N) ((Lam (Proj Fst (Asp (Sigma Entity (App (App (Var 1) (Var 0)) terminator))))), [])
   ("If","IN") -> mylex ["If"] "IN" (S [] `SL` S [] `SL` S []) (Lam (Lam (Lam (Pi (App (Var 2) terminator) (App (Var 2) (Var 1))))), [])
   --(w,"DT") -> mylex [w] "DT" (NP [] `SL` N) (nominalModifier w)
   -- ((T True 1 modifiableS `SL` (T True 1 modifiableS `BS` NP [F[Nc]])) `SL` N)   -- every, a
@@ -107,7 +110,6 @@ fromNLTKtoCCG (NLTKword word pos) = case (word,pos) of
   (w,"RB") -> mylex [w] "RB" ((S [] `BS` NP []) `BS` (S [] `BS` NP [])) (sid,[])
   ("and","CC") -> mylex ["and"] "CC" CONJ andSR
   ("or","CC") -> mylex ["or"] "CC" CONJ orSR
-  ("Either","CC") -> mylex ["Either"] "CC" ((S []) `SL` (S []) `SL` CONJ `SL` (S [])) ((UDTT.Lam (UDTT.Lam (UDTT.Lam (UDTT.Lam (UDTT.App (UDTT.App (Var 2) (UDTT.App (Var 3) (Var 0))) (UDTT.App (Var 1) (Var 0))))))),[])
   (w,",") -> mylex [w] "." (PUNCT) (id,[])
   (w,".") -> mylex [w] "." (PERIOD) (id,[])
   (w,_) -> []
